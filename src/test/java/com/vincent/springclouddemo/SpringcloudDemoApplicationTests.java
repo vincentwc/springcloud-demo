43920783147717f39759e4a7b4708443743b337e
vincentwc <1340476074@qq.com>
2021-04-09 14:12:08 +0800

    第一次提交微服务项目

---
+++ src/test/java/com/vincent/springclouddemo/SpringcloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.springclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
